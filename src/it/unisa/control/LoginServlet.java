360910ebe7ad3e6b21c4d3e1e5f436f5a2ef6c77
Francescopiobottaro <piobottaro300403@gmail.com>
2024-06-06 15:00:44 +0200

    ID 6 resolved

--- src/it/unisa/control/LoginServlet.java
+++ src/it/unisa/control/LoginServlet.java
@@ -11,53 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import it.unisa.model.*;
+import org.mindrot.jbcrypt.BCrypt;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-			
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	UserDao usDao = new UserDao();
-		
-		try
-		{	    
+    private static final long serialVersionUID = 1L;
 
-		     UserBean user = new UserBean();
-		     user.setUsername(request.getParameter("un"));
-		     user.setPassword(request.getParameter("pw"));
-		     user = usDao.doRetrieve(request.getParameter("un"),request.getParameter("pw"));
-			   		    
-		    
-		     String checkout = request.getParameter("checkout");
-		     
-		     if (user.isValid())
-		     {
-			        
-		          HttpSession session = request.getSession(true);	    
-		          session.setAttribute("currentSessionUser",user); 
-		          if(checkout!=null)
-		        	  response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
-		          
-		          else
-		        	  response.sendRedirect(request.getContextPath() + "/Home.jsp");
-		     }
-			        
-		     else 
-		          response.sendRedirect(request.getContextPath() +"/Login.jsp?action=error"); //error page 
-		} 
-				
-				
-		catch(SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-		  }
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserDao usDao = new UserDao();
+
+        try {
+            UserBean user = usDao.doRetrieveByUsername(request.getParameter("un"));
+            String password = request.getParameter("pw");
+            String checkout = request.getParameter("checkout");
+
+            if (user != null && BCrypt.checkpw(password, user.getPassword())) {
+                HttpSession session = request.getSession(true);
+                session.setAttribute("currentSessionUser", user);
+                if (checkout != null) {
+                    response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
+                } else {
+                    response.sendRedirect(request.getContextPath() + "/Home.jsp");
+                }
+            } else {
+                response.sendRedirect(request.getContextPath() + "/Login.jsp?action=error"); // error page
+            }
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+}
--- src/it/unisa/control/RegistrazioneServlet.java
+++ src/it/unisa/control/RegistrazioneServlet.java
@@ -1,9 +1,7 @@
 package it.unisa.control;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,54 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import it.unisa.model.*;
+import org.mindrot.jbcrypt.BCrypt;
 
-/**
- * Servlet implementation class RegistraziomeServlet
- */
-@WebServlet("/Registrazione")
-public class RegistrazioneServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+@WebServlet("/Register")
+public class RegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserDao usDao = new UserDao();
+        
+        try {
+            UserBean user = new UserBean();
+            user.setUsername(request.getParameter("un"));
+            user.setPassword(BCrypt.hashpw(request.getParameter("pw"), BCrypt.gensalt()));
+            // Imposta altri attributi dell'utente se necessario
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		UserDao dao = new UserDao();
-		String nome = request.getParameter("nome");
-		String cognome = request.getParameter("cognome");
-		String email = request.getParameter("email");
-		String dataNascita = request.getParameter("nascita");
-		String username = request.getParameter("us");
-		String pwd = request.getParameter("pw");
-
-        String[] parti = dataNascita.split("-");
-        dataNascita = parti[2] + "-" + parti[1] + "-" + parti[0];
-		
-		try {
-			
-			UserBean user = new UserBean();
-			user.setNome(nome);
-			user.setCognome(cognome);
-			user.setEmail(email);
-			user.setDataDiNascita(Date.valueOf(dataNascita));
-			user.setUsername(username);
-			user.setPassword(pwd);
-			user.setAmministratore(false);
-			user.setCap(null);
-			user.setIndirizzo(null);
-			user.setCartaDiCredito(null);
-			dao.doSave(user);
-			
-		}catch(SQLException e) {
-			System.out.println("Error:" + e.getMessage());
-		}
-				
-		response.sendRedirect(request.getContextPath() + "/Home.jsp");
-
-	}
-
+            usDao.registerUser(user);
+            response.sendRedirect(request.getContextPath() + "/Login.jsp");
+        } catch (SQLException e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
 }
